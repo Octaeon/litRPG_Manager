37ffa5a748b15101e50b73f5d654063f83936e71
Octaeon <octaeonspan@gmail.com>
2025-01-07 18:40:17 +0100

    Loading file for testings

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let filename = "test.txt";
+
+    println!("The file {filename} has these contents:");
+    let contents = fs::read_to_string(filename);
+
+    match contents {
+        Ok(a) => {
+            println!("{a}");
+        }
+        Err(e) => {
+            println!("Error! {e}")
+        }
+    }
 }
