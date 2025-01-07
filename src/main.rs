1f8cd5b3c1d607137749bb892e05ed6f1d0c80a5
Octaeon <octaeonspan@gmail.com>
2025-01-07 18:53:46 +0100

    Added input of the filename to be processed

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
+use std::env;
 use std::fs;
 
 fn main() {
-    let filename = "test.txt";
+    let args: Vec<String> = env::args().collect();
 
-    println!("The file {filename} has these contents:");
+    if args.len() < 2 {
+        println!("Input the name of the file as the first argument!");
+        return;
+    }
+
+    let filename = &args[1];
     let loaded_file = fs::read_to_string(filename);
 
     match loaded_file {
