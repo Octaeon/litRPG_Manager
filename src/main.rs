1e4a23d202177fe7a0b22627ff03cd838d51c1ee
Octaeon <octaeonspan@gmail.com>
2025-01-07 19:34:22 +0100

    Initial parsing of commands achieved

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,82 @@
+#![allow(non_snake_case)]
+
 use std::env;
 use std::fs;
 
-fn main() {
+#[derive(Debug, Clone)]
+enum Content {
+    Text(String),
+    Command(Command),
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Command {
+    AddOne,
+}
+
+fn main() -> Result<(), ()> {
     let args: Vec<String> = env::args().collect();
 
     if args.len() < 2 {
         println!("Input the name of the file as the first argument!");
-        return;
+        return Err(());
     }
 
-    let filename = &args[1];
+    let filename: &String = &args[1];
     let loaded_file = fs::read_to_string(filename);
 
     match loaded_file {
         Ok(contents) => {
-            println!("{contents}");
-            fs::write("output.txt", contents + "\ntest")
+            let output: Vec<Content> = parseFile(contents)?;
+
+            for chunk in output {
+                println!("{:?}", chunk);
+            }
+
+            fs::write("output.txt", "test")
                 .expect("For some reason, couldn't write the output to file");
+            Ok(())
         }
         Err(e) => {
-            println!("Error! {e}");
+            println!("{e}");
+            Err(())
         }
     }
+}
+
+fn parseFile(input: String) -> Result<Vec<Content>, ()> {
+    let mut result: Vec<Content> = vec![];
+
+    let mut reading_command: bool = false;
+    let mut buffer: String = "".to_string();
+
+    for char in input.chars() {
+        match char {
+            '$' => {
+                let chunk = if reading_command {
+                    parseCommand(buffer.clone())
+                } else {
+                    Ok(Content::Text(buffer.clone()))
+                };
+
+                result.push(chunk?);
+                reading_command = !reading_command;
+                buffer = "".to_string();
+            }
+            _ => buffer.push(char),
+        }
+    }
+
+    if reading_command {
+        // If all the characters have been read and we're still in the 'reading commands' state, it means that someone opened
+        // a command statement but didn't close it, so we throw an error.
+        Err(())
+    } else {
+        result.push(Content::Text(buffer));
+        Ok(result)
+    }
+}
+
+fn parseCommand(input: String) -> Result<Content, ()> {
+    Ok(Content::Command(Command::AddOne))
 }
