d58c2c10202e959171eea6e56e9c0d12929b061c
Octaeon <octaeonspan@gmail.com>
2025-01-11 23:16:12 +0100

    Created an integrated test for the whole process

--- src/tests.rs
+++ src/tests.rs
@@ -106,3 +106,30 @@ fn interpreterFunctionality() {
         Err(RunErr::TriedToInitializeExistingVariable)
     );
 }
+
+#[test]
+fn integrated() {
+    let whole_tests: Vec<(String, String)> = vec![
+        ("test.txt", "expected_output.txt"),
+        ("test.md", "expected_output.md"),
+    ]
+    .iter()
+    .map(|(a, b)| {
+        let path_to_tests = String::from("tests/");
+        (path_to_tests.clone() + a, path_to_tests + b)
+    })
+    .collect();
+
+    for (input, expect) in whole_tests {
+        let text_input = std::fs::read_to_string(input).expect("Couldn't read the input file");
+        let expected_output =
+            std::fs::read_to_string(expect).expect("Couldn't read the expected output file");
+
+        let mut storage: Storage = Storage::new();
+
+        let output = interpreter::run(&mut storage, parseFile(text_input).expect("Parsing error"))
+            .expect("Runtime error");
+
+        assert_eq!(expected_output, output);
+    }
+}
---
+++ tests/expected_output.md
@@ -0,0 +1,9 @@
+whatever, these are the contents of the file.
+
+
+
+the rest of the file
+
+
+
+here is TEST:7
